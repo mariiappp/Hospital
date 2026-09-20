100d5e61c1f5c5330aa42b7e231850eeef18a971
Maria Popova <mariiappp456ova@gmail.com>
2026-09-20 19:21:56 +0700

    Модель

---
+++ Hospital.Model/Doctor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital.Model
+{
+    public class Doctor
+    {
+        public int Id {  get; set; }
+        public string FullName { get; set; }
+        public string Specialization { get; set; }
+        public int Experience { get; set; }
+        public string Phone {  get; set; }
+        public int Office { get; set; }
+    }
+}
---
+++ Hospital.Model/Logic.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hospital.Model
+{
+    public class Logic
+    {
+        public List<Doctor> doctors = new List<Doctor>();
+
+        public void CreateDoctor(Doctor doctor)
+        {
+            doctors.Add(doctor);
+        }
+
+        public List<Doctor> GetDoctors() 
+        { 
+            return doctors; 
+        }
+
+        public bool DeleteDoctor(int id) 
+        {
+            Doctor doctor = doctors.FirstOrDefault(x => x.Id == id);
+            if (doctor == null) {
+                return false;
+            }
+            doctors.Remove(doctor);
+            return true;
+        }
+
+        public bool UpdateDoctor(Doctor updatedDoctor)
+        {
+            Doctor doctor = doctors.FirstOrDefault(x => x.Id == updatedDoctor.Id);
+            if (doctor == null)
+            {
+                return false;
+            }
+            doctor.FullName = updatedDoctor.FullName;
+            doctor.Specialization = updatedDoctor.Specialization;
+            doctor.Experience = updatedDoctor.Experience;
+            doctor.Phone = updatedDoctor.Phone;
+            doctor.Office = updatedDoctor.Office;
+            return true;
+        }
+
+        public Dictionary<string, List<Doctor>> GroupDoctorsBySpecialization()
+        {
+            return doctors
+                .GroupBy(x => x.Specialization)
+                .ToDictionary(x => x.Key, x => x.ToList());
+        }
+
+        public List<Doctor> GetDoctorsWithExperience(int minExperience)
+        {
+            return doctors
+                .Where(x =>  x.Experience >= minExperience)
+                .ToList();
+        }
+    }
+}
